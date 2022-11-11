81387f8e879344190c84ce56b5ff57ef5e33c532
haslerk <haslerklinsman@hotmail.com>
2022-11-11 00:22:25 -0600

    Segundo commit

---
+++ src/main/java/com/mycompany/java_learning/KlinsmannNunevo.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.java_learning;
+
+
+public class KlinsmannNunevo {
+    public static void main(String[] args) {
+        System.out.println("Prueba Hasler");
+    }
+    
+}
